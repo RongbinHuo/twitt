023b8fe315c6f5624aef37f13dd5f88347032bc8
Rongbin <rongbin@amg.tv>
2016-08-24 18:13:52 -0400

    first try

---
+++ sentiment.py
@@ -0,0 +1,60 @@
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
+
+# import twitter keys and tokens
+from config import *
+
+es = Elasticsearch()
+
+class TweetStreamListener(StreamListener):
+	# on success
+    def on_data(self, data):
+
+    	dict_data = json.loads(data)
+
+    	tweet = TextBlob(dict_data["text"])
+
+    	print tweet.sentiment.polarity
+
+    	if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+
+        print sentiment
+
+         es.index(index="sentiment",
+                 doc_type="test-type",
+                 body={"author": dict_data["user"]["screen_name"],
+                       "date": dict_data["created_at"],
+                       "message": dict_data["text"],
+                       "polarity": tweet.sentiment.polarity,
+                       "subjectivity": tweet.sentiment.subjectivity,
+                       "sentiment": sentiment})
+
+        return True
+
+    # on failure
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    # create instance of the tweepy tweet stream listener
+    listener = TweetStreamListener()
+
+    # set twitter keys/tokens
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    # create instance of the tweepy stream
+    stream = Stream(auth, listener)
+
+    # search twitter for "congress" keyword
+    stream.filter(track=['congress'])
