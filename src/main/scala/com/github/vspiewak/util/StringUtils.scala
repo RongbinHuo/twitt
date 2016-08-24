fb2288be66ce849a38650dd8761af67c00ca8e28
Rongbin <rongbin@amg.tv>
2016-08-24 17:41:50 -0400

    rebuild

--- src/main/scala/com/github/vspiewak/util/StringUtils.scala
+++
@@ -1,9 +0,0 @@
-package com.github.vspiewak.util
-
-object StringUtils {
-
-  def onlyWords(text: String) : String = {
-    text.split(" ").filter(_.matches("^[a-zA-Z0-9 ]+$")).fold("")((a,b) => a + " " + b).trim
-  }
-
-}
