33a040f873388184ff738b0ba8fafebc389e85f1
Rongbin <rongbin@amg.tv>
2016-08-28 15:48:21 -0400

    change training data

--- create_training_data.py
+++ create_training_data.py
@@ -1,4 +1,4 @@
-from googlefinance import getQuotes
+from yahoo_finance import Share
 import json
 from elasticsearch import Elasticsearch
 import time
@@ -9,10 +9,12 @@ import numpy as np
 date_now = int(time.time())
 date_passed_72_hours =  int(time.time()) - 72*60*60
 es = Elasticsearch()
+yahoo = Share('AMZN')
 # res = es.search(index='stocks',doc_type='Amazon', body={ "size": 0, "aggs": { "count_by_type": { "terms": { "field": '_type'}}}})
 res = es.search(index='stocks',doc_type='Amazon', body={ "size": 0, "aggs": { "avg_grade": { "avg": { "field": 'scoring'}}}})
 avg_score_all_data = res["aggregations"]["avg_grade"]["value"]
-
+stock_year_high = yahoo.get_year_high()
+stock_year_low = yahoo.get_year_low()
 # res = es.search(index='stocks',doc_type='Amazon', body={ "size": 0, "aggs": { "min_time": { "min": { "field": 'created_at'}}}})
 # min_timestamp = res["aggregations"]["min_time"]["value"]
 # res = es.search(index='stocks',doc_type='Amazon', body={ "size": 0, "aggs": { "max_time": { "max": { "field": 'created_at'}}}})
@@ -29,10 +31,15 @@ for data in dataset:
 	score_range_avg = res["aggregations"]["avg_grade"]["value"]
 	res = es.search(index='stocks',doc_type='Amazon', body={ "size": 0, "query": { "range": { "created_at": {"lte": time_range_start}}}, 
 		  "aggs": { "avg_grade": { "avg": { "field": 'scoring'}}}})
-	score_range_avg_pre = res["aggregations"]["avg_grade"]["value"]
+	score_range_avg_pre = avg_score_all_data if res["aggregations"]["avg_grade"]["value"] == None else res["aggregations"]["avg_grade"]["value"]
 	quote_data = data[1]
+	quote_data_year_high_percent = (stock_year_high - quote_data)/quote_data
+	quote_data_year_low_percent = (quote_data - stock_year_low)/stock_year_low
+	quote_data_increase = (quote_data-original_quote)/original_quote
+	scoring_increase_overall = (score_range_avg-avg_score_all_data)/avg_score_all_data
+	scoring_increase_than_pre = (score_range_avg-score_range_avg_pre)/score_range_avg_pre
 	if score_range_avg > 0:
-		train_data = original_quote, score_range_avg_pre, score_range_avg, avg_score_all_data, quote_data
+		train_data = quote_data_year_percent, scoring_increase_overall, scoring_increase_than_pre, quote_data_increase
 		train_arry = np.array(train_data)
 		with open(r'./data/train_data.csv', 'a') as f:
 			f.write(",".join(map(str, train_arry))+'\n')
--- training.py
+++ training.py
@@ -4,12 +4,47 @@ from math import sqrt
 from pybrain.datasets.supervised import SupervisedDataSet as SDS
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure import TanhLayer
 
 
-train_file = 'data/train.csv'
+train_file = 'data/train_data.csv'
 validation_file = 'data/validation.csv'
 output_model_file = 'model/model.pkl'
 
-hidden_size = 100
+hidden_size = 30
 epochs = 600
 
+
+train = np.loadtxt( train_file, delimiter = ',' )
+validation = np.loadtxt( validation_file, delimiter = ',' )
+train = np.vstack(( train, validation ))
+x_train = train[:,0:-1]
+y_train = train[:,-1]
+y_train = y_train.reshape( -1, 1 )
+
+input_size = x_train.shape[1]
+target_size = y_train.shape[1]
+
+ds = SDS( input_size, target_size )
+ds.setField( 'input', x_train )
+ds.setField( 'target', y_train )
+
+net = buildNetwork( input_size, hidden_size, target_size, hiddenclass=TanhLayer, bias = True )
+trainer = BackpropTrainer( net,ds )
+
+print "training for {} epochs...".format( epochs )
+
+for inp, tar in ds:
+    print [net.activate(inp), tar]
+
+trainer.trainUntilConvergence(maxEpochs=1000, continueEpochs=10, validationProportion=0.1)
+
+for inp, tar in ds:
+    print [net.activate(inp), tar]
+
+# for i in range( epochs ):
+# 	mse = trainer.train()
+# 	rmse = sqrt( mse )
+# 	print "training RMSE, epoch {}: {}".format( i + 1, rmse )
+
+pickle.dump( net, open( output_model_file, 'wb' ))
