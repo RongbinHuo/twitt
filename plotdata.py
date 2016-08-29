0c2784d8e16c8a06d3892e47ba873e9447e38b57
Rongbin <rongbin@amg.tv>
2016-08-29 15:53:40 -0400

    create function to draw plots

---
+++ plotdata.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+train_file = 'data/train_data.csv'
+
+
+train = np.loadtxt( train_file, delimiter = ',' )
+
+x_train = train[:,0:-1]
+y_train = train[:,-1]
+y_train = y_train.reshape( -1, 1 )
+
+x_plot = []
+for x in x_train:
+	x_plot.append(x[1])
+
+y_plot = []
+for y in y_train:
+	y_plot.append(y[0])
+
+plt.plot(x_plot,y_plot)
+
+plt.show()
