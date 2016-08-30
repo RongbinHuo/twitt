a5b9a0e2160b4c4424cecfa41af067352a152adc
Rongbin <rongbin@amg.tv>
2016-08-30 16:02:47 -0400

    handle error

--- ingest/ingest_amazon.py
+++ ingest/ingest_amazon.py
@@ -67,6 +67,8 @@ if __name__ == '__main__':
 
     # create instance of the tweepy stream
     stream = Stream(auth, listener)
-
-    # search twitter for "congress" keyword
-    stream.filter(track=[stock])
+    try:
+        # search twitter for "congress" keyword
+        stream.filter(track=[stock])
+    except Exception, e:
+        pass
