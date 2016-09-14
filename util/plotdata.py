dcf43c1a7ea5ab472955e52d7e821ca4d8f75d23
Rongbin <rongbin@amg.tv>
2016-09-13 23:06:14 -0400

    add graphlab key

--- util/plotdata.py
+++ util/plotdata.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pybrain.datasets.supervised import SupervisedDataSet as SDS
 
-train_file = '../data/train_data.csv'
+train_file = '../data/view_data.csv'
 
 
 train = np.loadtxt( train_file, delimiter = ',' )
@@ -34,17 +34,17 @@ n1=0
 m1=0
 for inp, tar in ds:
 	c = c+1
-	if inp[1] > 0.2 and tar[0] > 0:
+	if inp[0] > 0 and tar[0] > 0:
 		n=n+1
-	if inp[1] < -0.2 and tar[0] < 0:
+	if inp[0] < 0 and tar[0] < 0:
 		m=m+1
-	if inp[1] > 0.2 and tar[0] < 0:
+	if inp[0] > 0 and tar[0] < 0:
 		n1=n1+1
-	if inp[1] < -0.2 and tar[0] > 1.0:
+	if inp[0] < 0 and tar[0] > 0:
 		m1=m1+1
 print 'Test result, total {} records, {} is correct and {} is wrong. Perce {}'.format(c,n+m, n1+m1, float(n+m)/float(n+m+n1+m1))
 
-plt.plot(x_plot,y_plot)
+# plt.plot(x_plot,y_plot,marker='.')
 
 
-plt.show()
+# plt.show()
